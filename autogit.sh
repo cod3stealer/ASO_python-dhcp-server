e272e3a4cfe581dfc05824e6434528a41765357f
cod3stealer <sromerodiaz@dnaielcastelao.org>
2023-03-18 18:40:16 +0100

    virtualenv for python

--- autogit.sh
+++ autogit.sh
@@ -4,10 +4,12 @@ git branch
 
 git remote -v
 
+echo -e '\n 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'
+
 git config  --global user.email "sromerodiaz@dnaielcastelao.org"
 git config  --global user.name "cod3stealer"
 
-git add .
+git add *
 read -p "Introduce el commit: " msg
 git commit -m "$msg"
 git push -u origin main
