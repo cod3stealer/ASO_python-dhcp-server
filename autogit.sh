f4548b50c4a308d31a10595b4065fbbaf5f273a1
cod3stealer <sromerodiaz@dnaielcastelao.org>
2023-03-18 18:25:42 +0100

    a

--- autogit.sh
+++ autogit.sh
@@ -3,7 +3,7 @@
 git config  --global user.email "sromerodiaz@dnaielcastelao.org"
 git config  --global user.name "cod3stealer"
 
-git add $PWD
+git add .
 read -p "Introduce el commit: " msg
 git commit -m "$msg"
 git push -u origin main
