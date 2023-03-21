7867d3faec65f1ddfeb653d8c77d497fb20e7da9
cod3stealer <sromerodiaz@dnaielcastelao.org>
2023-03-21 21:04:01 +0100

    proyecto terminado

--- main.py
+++ main.py
@@ -22,6 +22,14 @@ def root():
     if os.geteuid() != 0:
         print(bcolors.WARNING + "Para ejecutar este script son necesarios los permisos de superusuario o 'root'!" + bcolors.ENDC)
         exit(1)
+    else:
+        print("Este script instalará el paquete ISC-DHCP-SERVER y manipulará varíos archivos de configuración\nDe esta manera, el script despliega un servicio DHCP sobre tu maquina")
+        resp=input("Estás de acuerdo con esto? (si) / (no) ")
+        while resp != "no" and resp != "si":
+            resp=input("Estás de acuerdo con esto? (si) / (no) ")
+        if  resp == "no":
+            print("Saliendo del programa...")
+            exit(1)
 
 # ! prerequisitos() comprueba que el paquete de Linux DHCP-server está instalado
 def prerequisitos_dhcp():
@@ -52,49 +60,49 @@ def prerequisitos_sockets():
                                "escribiendo el nombre EXACTO de una de ellas" + bcolors.ENDC)
         con = 0
         while con == 0:
-            inet_dhcp = input("=> ")
-            for i in interfaz.splitlines():
-                if inet_dhcp == i:
-                    con = 1
-
-        print(bcolors.HEADER + "\n\nLa interfaz se ha recogido correctamente:\n" + bcolors.HEADER + inet_dhcp + bcolors.ENDC + "\nCon IP" + bcolors.HEADER + os.popen(
-                'ifconfig | grep ' + inet_dhcp + ' -A 1 | cut -d" " -f10').read() + bcolors.ENDC+bcolors.HEADER+"\nAHORA, selecciona una de las interfaces anteriores para que sea el encargado de dar conectividad a Internet\n"
-                               "a los clientes escribiendo el nombre EXACTO de una de ellas." + bcolors.ENDC)
-
-        interfaz = os.popen('nmcli dev | grep -v '+inet_dhcp+' | grep -w "conectado\|desconectado\|connected\|disconnected" | cut -d" " -f1,5').read()
-
-        for i in interfaz.splitlines():
-            print("\/ " + i + " \/" + os.popen('ifconfig | grep "' + i + '" -A 1 | cut -d" " -f10').read())
-        print("AVISO: si escoges una interfaz de red desconectada, esta se activará automáticamente")
-
-        interfaz = os.popen('nmcli dev | grep -v "' + inet_dhcp + '" | grep -w "conectado\|desconectado\|connected\|disconnected" | cut -d" " -f1').read()
-        con = 0
-        while con == 0:
             inet_internet = input("=> ")
             for i in interfaz.splitlines():
                 if inet_internet == i:
                     con = 1
 
+        print(bcolors.HEADER + "\n\nLa interfaz se ha recogido correctamente:\n" + bcolors.HEADER + inet_internet + bcolors.ENDC + "\nCon IP" + bcolors.HEADER + os.popen(
+                'ifconfig | grep ' + inet_internet + ' -A 1 | cut -d" " -f10').read() + bcolors.ENDC+bcolors.HEADER+"\nAHORA, selecciona una de las interfaces anteriores para que sea el encargado de dar conectividad a Internet\n"
+                               "a los clientes escribiendo el nombre EXACTO de una de ellas." + bcolors.ENDC)
+
+        interfaz = os.popen('nmcli dev | grep -v '+inet_internet+' | grep -w "conectado\|desconectado\|connected\|disconnected" | cut -d" " -f1,5').read()
+
+        for i in interfaz.splitlines():
+            print("\/ " + i + " \/" + os.popen('ifconfig | grep "' + i + '" -A 1 | cut -d" " -f10').read())
+        print("AVISO: si escoges una interfaz de red desconectada, esta se activará automáticamente")
+
+        interfaz = os.popen('nmcli dev | grep -v "' + inet_internet + '" | grep -w "conectado\|desconectado\|connected\|disconnected" | cut -d" " -f1').read()
+        con = 0
+        while con == 0:
+            inet_dhcp = input("=> ")
+            for i in interfaz.splitlines():
+                if inet_dhcp == i:
+                    con = 1
+
         # Cambio de estado de las interfaces
-        result = os.popen('ip link show '+inet_internet+' up').read()
+        result = os.popen('ip link show '+inet_dhcp+' up').read()
         if result == "":
-            result = os.popen('ip link set '+inet_internet+' up').read()
+            result = os.popen('ip link set '+inet_dhcp+' up').read()
             if result != 0:
-                print(f"Error en: {inet_internet}.")
+                print(f"Error en: {inet_dhcp}.")
                 exit(1)
 
         # Configuracion IP
-        ip_internet = "192.168.71.1"
-        os.popen('sudo ip addr flush dev '+inet_internet)
-        result = os.popen('sudo ip addr add '+ip_internet+'/24 dev '+inet_internet).read()
+        ip_dhcp = "192.168.71.1"
+        os.popen('sudo ip addr flush dev '+inet_dhcp)
+        result = os.popen('sudo ip addr add '+ip_dhcp+'/24 dev '+inet_dhcp).read()
         if result != "":
-            print(f"Error: IP {ip_internet} en {inet_internet}.")
+            print(f"Error: IP {ip_dhcp} en {inet_internet}.")
             exit(1)
 
-        print(f"IP de {inet_internet} cambiada a {ip_internet}.")
+        print(f"IP de {inet_dhcp} cambiada a {ip_dhcp}.")
         print(os.popen('ifconfig | grep "' + inet_internet + '" -A 1 | cut -d" " -f1,10').read())
 
-        return inet_dhcp,inet_internet
+        return inet_internet, inet_dhcp
     else:
         print(bcolors.FAIL+"Para lanzar este script es necesario tener dos interfaces de red activas!!"+bcolors.ENDC)
         exit(1)
@@ -104,13 +112,13 @@ def prerequisitos_sockets():
 
 # ! set_inetv4() establece la interfaz que dará IPs
 def set_inetv4(interfaz):
-    print("Configurando archivos de servidor...")
     with open('/etc/default/isc-dhcp-server', 'w', encoding='utf-8') as file:
         file.write('INTERFACESv4="' + interfaz + '"\nINTERFACESv6=""')
 
 # ! set_dhcp_conf() establece el rango de IPs dadas por el DHCP
 def set_dhcp_conf(IP):
     sub=get_subnet(IP)
+    sub = str(sub)
     subnet = sub + "0"
     netmask = "255.255.255.0"
     range_start = sub + "2"
@@ -125,15 +133,15 @@ def set_dhcp_conf(IP):
     with open("/etc/dhcp/dhcpd.conf", "w") as f:
         f.write("default-lease-time {};\n".format(default_lease))
         f.write("max-lease-time {};\n".format(max_time))
-        f.write("authoritative;")
-        f.write("subnet {} netmask {} {\n".format(subnet, netmask))
+        f.write("authoritative;\n")
+        f.write("subnet {} netmask {}".format(subnet, netmask))
+        f.write("{\n")
         f.write("range {} {};\n".format(range_start, range_end))
         f.write("option routers {};\n".format(gateway))
-        f.write("option domain-name-servers {};\n}".format(", ".join(dns_servers)))
+        f.write("option domain-name-servers {};\n".format(", ".join(dns_servers)))
+        f.write("}")
 
-    os.popen('sudo systemctl start isc-dhcp-server')
-    os.popen('sudo systemctl enable isc-dhcp-server')
-    os.popen('sudo systemctl status isc-dhcp-server')
+    os.popen('sudo service isc-dhcp-server start')
     os.popen('sudo netplan apply')
     allow_ufw()
 
@@ -161,22 +169,25 @@ def overw(fil,word,wrx,adds):
 
 # ! set_static_dhcp() modifica Netplan para que esté establezca el servidor DHCP con IP estática
 def set_static_dhcp(net,inet,inet_internet):
+    net = net.strip()
+    inet_internet = inet_internet.strip()
     netmanager = [
         "# Let NetworkManager manage all devices on this system",
         "network:",
-        " ethernets:",
-        "  " + inet + ":",
-        "   dhcp4: false",
-        "   addresses: [" + net + "/24]",
-        "   nameservers:",
-        "	  addresses: [8.8.8.8,8.8.4.4]",
-        "  routes:",
-        "	- to: default",
-        "  	  via: "+inet_internet,
-        " version: 2",
-        " renderer: NetworkManager"
+        "  ethernets:",
+        "    " + inet + ":",
+        "     addresses: [" + net + "/24]",
+        "     dhcp4: false",
+        "     nameservers:",
+        "       addresses: [8.8.8.8, 8.8.4.4]",
+        "     routes:",
+        "      - to: 0.0.0.0/0",
+        "        via: "+inet_internet,
+        "  version: 2",
+        "  renderer: NetworkManager"
     ]
     overw("/etc/netplan/01-network-manager-all.yaml", netmanager, "w+", "\n")
+
     os.popen('sudo netplan apply')
 
 
@@ -188,8 +199,13 @@ def __MAIN__():
     print("Comprando interfaces de red...")
     inet = prerequisitos_sockets()
     IP = os.popen('ifconfig | grep ' + inet[0] + ' -A 1 | cut -d" " -f10').read()
+    IP2 = os.popen('ifconfig | grep ' + inet[1] + ' -A 1 | cut -d" " -f10').read()
+    print("Configurando archivos de servidor...")
     set_inetv4(inet[0])
-    set_static_dhcp(IP,inet[0],inet[1])
+    print("Asignando IP estática...")
+    set_static_dhcp(IP,inet[0],IP2)
+    print("Finalizando...")
     set_dhcp_conf(IP)
+    print(bcolors.HEADER+"Servidor desplegado con éxito"+bcolors.ENDC)
 
 __MAIN__()
--- pruebas.py
+++ pruebas.py
@@ -12,69 +12,18 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+def get_subnet(ip):
+    subnet_regex = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.')
+    subnet_match = subnet_regex.match(ip)
+    subnet = subnet_match.group() if subnet_match else None
+    return subnet
 
-def prerequisitos_sockets():
-    interfaz = os.popen('nmcli dev | grep -w "conectado\|connected" | cut -d" " -f1').read()
-    c = 0
-    for _ in interfaz.splitlines():
-        c += 1
-    if c > 1:
-        print("Interfaces de red en orden!" + bcolors.HEADER + "\nEstas son tus interfaces activas: " + bcolors.ENDC)
-        for i in interfaz.splitlines():
-            print("\/ " + i + " \/" + os.popen('ifconfig | grep "' + i + '" -A 1 | cut -d" " -f10').read())
-        print(bcolors.HEADER + "Selecciona una de las interfaces anteriores para\n"
-                               "que sea el encargado de dar IPs a los demás clientes\n"
-                               "escribiendo el nombre EXACTO de una de ellas" + bcolors.ENDC)
-        con = 0
-        while con == 0:
-            inet_dhcp = input("=> ")
-            for i in interfaz.splitlines():
-                if inet_dhcp == i:
-                    con = 1
-
-        print(bcolors.HEADER + "\n\nLa interfaz se ha recogido correctamente:\n" + bcolors.HEADER + inet_dhcp + bcolors.ENDC + "\nCon IP" + bcolors.HEADER + os.popen(
-                'ifconfig | grep ' + inet_dhcp + ' -A 1 | cut -d" " -f10').read() + bcolors.ENDC+bcolors.HEADER+"\nAHORA, selecciona una de las interfaces anteriores para que sea el encargado de dar conectividad a Internet\n"
-                               "a los clientes escribiendo el nombre EXACTO de una de ellas." + bcolors.ENDC)
-
-        interfaz = os.popen('nmcli dev | grep -v '+inet_dhcp+' | grep -w "conectado\|desconectado\|connected\|disconnected" | cut -d" " -f1,5').read()
-
-        for i in interfaz.splitlines():
-            print("\/ " + i + " \/" + os.popen('ifconfig | grep "' + i + '" -A 1 | cut -d" " -f10').read())
-        print("AVISO: si escoges una interfaz de red desconectada, esta se activará automáticamente")
-
-        interfaz = os.popen('nmcli dev | grep -v "' + inet_dhcp + '" | grep -w "conectado\|desconectado\|connected\|disconnected" | cut -d" " -f1').read()
-        con = 0
-        while con == 0:
-            inet_internet = input("=> ")
-            for i in interfaz.splitlines():
-                if inet_internet == i:
-                    con = 1
-
-        # Bring up the interface if it's not up
-        result = os.popen('ip link show '+inet_internet+' up').read()
-        if result == "":
-            result = os.popen('ip link set '+inet_internet+' up').read()
-            if result != 0:
-                print(f"Error en: {inet_internet}.")
-                exit(1)
-
-        # Configure the IP address
-        ip_internet = "192.168.71.1"
-        os.popen('sudo ip addr flush dev '+inet_internet)
-        result = os.popen('sudo ip addr add '+ip_internet+'/24 dev '+inet_internet).read()
-        if result != "":
-            print(f"Error: IP {ip_internet} en {inet_internet}.")
-            exit(1)
-
-        print(f"IP de {inet_internet} cambiada a {ip_internet}.")
-        print(os.popen('ifconfig | grep "' + inet_internet + '" -A 1 | cut -d" " -f1,10').read())
-
-        return inet_dhcp,inet_internet
-    else:
-        print(bcolors.FAIL+"Para lanzar este script es necesario tener dos interfaces de red activas!!"+bcolors.ENDC)
-        exit(1)
-    # Saber si hay dos sockets activos:
-    # Socket DHCP ==> Red Interna, guardar su IP y su nombre en una variable
-    # Socket salida INTERNET ==> NAT, guardar su IP y su nombre en una variable
-
-inet = prerequisitos_sockets()
+IP = "12.412.51.2"
+sub=get_subnet(IP)
+sub = str(sub)
+print(sub)
+subnet = sub + "0"
+print(subnet)
+netmask = "255.255.255.0"
+range_start = sub + "2"
+range_end = sub + "200"
