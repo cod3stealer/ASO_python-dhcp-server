e08b3e7d11d3274c4b4eb4aedc3d07f5913dd42a
cod3stealer <sromerodiaz@dnaielcastelao.org>
2023-03-21 18:34:39 +0100

    a

--- autogit.sh
+++ autogit.sh
@@ -4,6 +4,8 @@ git branch
 
 git remote -v
 
+git remote add origin https://github.com/cod3stealer/ASO_python-dhcp-server
+
 cat /home/saitama/tok.txt
 
 git config  --global user.email "sromerodiaz@dnaielcastelao.org"
